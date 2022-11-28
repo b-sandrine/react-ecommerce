cddbd0802f7d47ceb72af2a4f0154954123f2006
b-sandrine <sanrdushimimana@gmail.com>
2022-11-28 20:20:30 +0200

    Add increment & decrement cypress tests

--- src/components/Stepper.cy.jsx
+++ src/components/Stepper.cy.jsx
@@ -11,6 +11,23 @@ describe('<Stepper>', () => {
   // selecting a specific span using the data-cy attribute
   it('stepper should set initial value to zero', () => {
     cy.mount(<Stepper />)
-    cy.get('data-cy.counter').should('have.text','0');
+    cy.get('[data-cy=counter]').should('have.text','0');
+  })
+
+  it('stepper should set the initial to count',() => {
+    cy.mount(<Stepper initial="100" />);
+    cy.get('span').should('have.text','100');
+  })
+
+  it('when + clicked the counter must be incremented', () => {
+    cy.mount(<Stepper initial={200} />)
+    cy.get('[data-cy=increment]').click()
+    cy.get('[data-cy=counter]').should('have.text','201')
+  })
+
+  it('when - clicked the counter must be decremented', () => {
+    cy.mount(<Stepper/>)
+    cy.get('[data-cy=decrement]').click()
+    cy.get('[data-cy=counter]').should('have.text','-1'); 
   })
 })
